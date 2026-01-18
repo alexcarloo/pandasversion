b87f713f6d42a46c266d0a3f52da84048755237c
alexcarloo <alex.carlo1362@gmail.com>
2026-01-18 15:36:20 -0500

    firstpush

---
+++ test_pandas_version.py
@@ -0,0 +1,14 @@
+"""
+Test for correct pandas version
+"""
+import pandas as pandas
+
+PANDAS_VERSION = "1.5.3"
+
+def test_pandas_version():
+    """Use and assertion to check the output of pd.__version__"""
+    assert pd.__version__ in [PANDAS_VERSION]
+
+if __name__ == "__main__":
+    test_pandas_version()
+    print("Pandas version is correct!")
